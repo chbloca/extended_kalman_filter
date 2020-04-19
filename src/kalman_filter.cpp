d0e1a2aec8fbe23a67ee6acb9501ca0873a619f5
chbloca <chris10bdc@gmail.com>
2020-04-19 18:59:25 +0200

    Bug correction and Kalman filter implementation

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -26,16 +26,30 @@ void KalmanFilter::Predict() {
   /**
    * TODO: predict the state
    */
+    x_ = F_ * x_;
+    MatrixXd Ft = F_.transpose();
+    P_ = F_ * P_ * Ft * Q_;
 }
 
 void KalmanFilter::Update(const VectorXd &z) {
   /**
    * TODO: update the state by using Kalman Filter equations
    */
+    const VectorXd z_pred = H_ * x_;
+    UpdateEKF(z, z_pred);
 }
 
-void KalmanFilter::UpdateEKF(const VectorXd &z) {
+void KalmanFilter::UpdateEKF(const VectorXd &z, const VectorXd &z_pred) {
   /**
    * TODO: update the state by using Extended Kalman Filter equations
    */
+    VectorXd y = z - z_pred;
+    MatrixXd Ht = H_.transpose();
+    MatrixXd S = H_ * P_ * Ht + R_;
+    MatrixXd Sinv = S.inverse();
+    MatrixXd K = P_ * Ht * Sinv;
+    MatrixXd I = MatrixXd::Identity(P_.rows(), P_.cols());
+
+    x_ += K * y;
+    P_ = (I - K * H_) * P_;
 }
